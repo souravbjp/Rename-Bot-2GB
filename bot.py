bace50f31c054e987494407a09b7282860b0fd85
souravbjp <souravbosu.bjp@zohomail.com>
2026-03-25 22:35:28 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from config import Config
 from aiohttp import web
+from pyromod import listen
 
 routes = web.RouteTableDef()
 
